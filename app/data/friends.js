5c90f736fa0e44e005d0c0ae0b9ba17507e35748
Ben Wilson <benjamin.j.wilson1990@gmail.com>
2019-07-06 00:26:29 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+
+var friendsList = [
+    {
+        name: "Chin Chin",
+        photo: "https://i0.wp.com/zamzows.com/wp-content/uploads/2015/11/chinchilla.jpg?resize=560%2C450&ssl=1",
+        // 10 different scores from 1 to 5
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "insert name",
+        photo: "insert photo.jpg",
+        scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    }
+];
+
+module.exports = friendsList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+// Your `apiRoutes.js` file should contain two routes:
+
+//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+
+var friendsData = require("../data/friends");
+
+module.exports = function(app) {
+    // API GET Requests
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+
+    // API POST Requests
+    app.post("/api/friends", function(req, res) {
+        res.json(friendsData);
+    });
+};
